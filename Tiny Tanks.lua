264ceb5c8bef4013fb30146da4bdf7a42e78ab41
Frindow <69374353+Frindow@users.noreply.github.com>
2022-08-14 16:56:41 -0400

    Update Tiny Tanks.lua

--- Tiny Tanks.lua
+++ Tiny Tanks.lua
@@ -5,7 +5,8 @@ BulletsPerShot = 1				MaxFireRate = 0.3
 ConsecutiveReloadTime = 0.675			MoveSpeed = 24
 DriveSound = "TankTreads"			ReloadTime = 1
 ExplodeDirection = Vector3.new(0, 0, 0)		RotationSpeed = 140
-MaxLoadedSpecial = 4
+MaxLoadedSpecial = 4				TeleportingBase = "Red"
+TeleportOnExecute = true
 
 for i, tank in pairs(workspace.Tanks:GetChildren()) do
 	if tank.Name.find(game.Players.LocalPlayer.Name, game.Players.LocalPlayer.Name) then
@@ -20,14 +21,26 @@ for i, tank in pairs(workspace.Tanks:GetChildren()) do
 		tank.Settings.RotationSpeed.Value = RotationSpeed
 		tank.Settings.MaxLoadedSpecial.Value = MaxLoadedSpecial
 		tank.Settings.SpecialAmmoSpeed.Value = SpecialAmmoSpeed
---		tank.Settings.MineMaxDetectionDistance.Value = MineMaxDetectionDistance
---		tank.Settings.KillText.Value = KillText
---		tank.Settings.AbilityCooldown.Value = AbilityCooldown
---		tank.Settings.HasCrown.Value = HasCrown
---		tank.Settings.MineTransparency.Value = MineTransparency
+		--		tank.Settings.MineMaxDetectionDistance.Value = MineMaxDetectionDistance
+		--		tank.Settings.KillText.Value = KillText
+		--		tank.Settings.AbilityCooldown.Value = AbilityCooldown
+		--		tank.Settings.HasCrown.Value = HasCrown
+		--		tank.Settings.MineTransparency.Value = MineTransparency
 --[[		for i, ability in pairs(tank.Abilities:GetChildren()) do
 			ability.CooldownTime.Value = AbilityCooldown
 		end--]]
+	end
+	
+	if TeleportOnExecute == true then
+		for i, map in pairs(workspace:GetChildren()) do
+			if map.Name.find(map.Name, "Map") then
+				if TeleportingBase == "Red" then
+					tank.Settings.Position.Value = Vector3.new(29.5, 14.5, -280)
+				else
+					tank.Settings.Position.Value = Vector3.new(29.5, 14.5, 306)
+				end
+			end
+		end
 	end
 end
 
